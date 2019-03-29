e37af3f917f4f052b22085da5fb73e46c0ebc47d
Brad Hammond <brad@xy01.net>
2019-03-29 17:50:24 +1100

    Added NDI and started making shader mixer

---
+++ Assets/Shader Mixer/ShaderObject.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShaderObject : MonoBehaviour
+{
+    public Material _Mat;
+    public float _Scale;
+    public Color _BaseCol;
+
+    public Slider _ScaleSlider;
+
+    private void Awake()
+    {
+        _Mat = GetComponent<Renderer>().material;
+        _ScaleSlider.value = transform.localScale.x;
+    }
+
+
+    public void UpdateFloat(string name, float val)
+    {
+        print(name + "    Setting value: " + name + "   " + val);
+        _Mat.SetFloat(name, val);
+    }
+
+    public void SetScale(float scale)
+    {
+        transform.localScale = Vector3.one * scale;
+    }
+}
---
+++ Assets/Shader Mixer/ShaderPropFloat.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+[System.Serializable]
+public class ShaderPropFloat : MonoBehaviour
+{
+    public ShaderObject _Object;
+    public string _Name;
+    public float _Value;
+
+    private void Start()
+    {
+        _Value = _Object._Mat.GetFloat(_Name);
+
+        Slider s  = GetComponentInChildren<Slider>();
+        if(s !=null)
+        {
+            print("Setting value " + _Value);
+            s.value = _Value;
+        }
+    }
+
+    public void UpdateValue(float val)
+    {
+        _Value = val;
+        _Object.UpdateFloat(_Name, _Value);
+    }
+}

c1f6c3f87365f0587fd41378d49cc1fa4789e24d
Brad Zenbook <brad@exp.org.au>
2019-06-12 14:24:49 +0100

    adding sphere controller

---
+++ Assets/_Project/_Sandbox/Glastonbury looks/CoordinateConverter.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class CoordinateConverter
+{
+    public static float[] CartesianToSpherical(float x, float y, float z)
+    {
+        float[] retVal = new float[2];
+
+        if (x == 0)
+        {
+            x = Mathf.Epsilon;
+        }
+        retVal[0] = Mathf.Atan(z / x);
+
+        if (x < 0)
+        {
+            retVal[0] += Mathf.PI;
+        }
+
+        retVal[1] = Mathf.Asin(y / Terrain.RADIUS);
+
+        return retVal;
+    }
+
+    public static float[] CartesianToSpherical(Vector3 v)
+    {
+        return CartesianToSpherical(v.x, v.y, v.z);
+    }
+
+
+    public static Vector3 SphericalToCartesian(float latitude, float longitude)
+    {
+        float a = Terrain.RADIUS * Mathf.Cos(longitude);
+        float x = a * Mathf.Cos(latitude);
+        float y = Terrain.RADIUS * Mathf.Sin(longitude);
+        float z = a * Mathf.Sin(latitude);
+
+        return new Vector3(x, y, z);
+    }
+
+}
---
+++ Assets/_Project/_Sandbox/Glastonbury looks/SphereController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Control input points on a spehre using Mouse or VR
+/// </summary>
+public class SphereController : MonoBehaviour
+{
+    public Transform[] _InputTransforms;
+
+    public enum Mirror
+    {
+        None,
+        LeftRight,
+        TopBottom,
+        QuadrantsAroundY,
+    }
+
+    public enum ControlSource
+    {
+        Mouse,
+        VR,
+    }
+
+    public Mirror _Mirror = Mirror.None;
+    public ControlSource _ControlSource = ControlSource.Mouse;
+
+    private void Start()
+    {
+        
+    }
+
+
+}

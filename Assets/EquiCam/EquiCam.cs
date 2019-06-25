09f905f40436665e6bb6f06a28371f64e543af20
Brad Hammond <brad@xy01.net>
2019-06-25 18:02:12 +0100

    added crystal subsatance and fixed culling on equirect camera

--- Assets/EquiCam/EquiCam.cs
+++ Assets/EquiCam/EquiCam.cs
@@ -53,6 +53,10 @@ namespace BodhiDonselaar
             {
                 _ProjectionMat.SetTexture("_MainTex", _CubemapRT);
             }
+
+            _Cam.cullingMatrix = Matrix4x4.Ortho(-99999, 99999, -99999, 99999, 0.001f, 99999) *
+                           Matrix4x4.Translate(Vector3.forward * -99999 / 2f) *
+                           _Cam.worldToCameraMatrix;
         }
 
 		void OnDisable()
